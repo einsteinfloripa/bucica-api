9b456608066b46152231ffa7eddddde2073705e0
Marcelo Ecco <marceloecco1@gmail.com>
2023-03-06 11:36:51 -0300

    chore: novemente comita mudanças para poder dar ustash em mudanças feitas na branch develop diretamente

--- src/main.py
+++ src/main.py
@@ -8,3 +8,20 @@ app = FastAPI()
 @app.get("/")
 def read_root() -> Dict[str, str]:
     return {"Hello": "World"}
+
+
+"""
+from fastapi import FastAPI
+
+from routers import foo
+
+app = FastAPI()
+
+
+@app.exception_handler(AppExceptionCase)
+async def custom_app_exception_handler(request, e):
+    return await app_exception_handler(request, e)
+
+
+app.include_router(foo.router)
+"""
