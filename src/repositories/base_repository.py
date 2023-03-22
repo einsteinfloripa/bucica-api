0a8eab978c4446b4548ea7e88f3ced702112f32a
Marcelo Ecco <marceloecco1@gmail.com>
2023-03-22 17:27:36 -0300

    style: add docstring to base_repository functions

--- src/repositories/base_repository.py
+++ src/repositories/base_repository.py
@@ -4,10 +4,26 @@ from src.database.session import DBSessionMixin
 
 
 class AppRepository(DBSessionMixin):
-    def get_first(self, **kwargs) -> Any:
+    def get_first(self, **kwargs) -> Any | None:
+        """
+        Get first object from database using the specified kwargs as filters
+
+        The object kwarg is required to specify wich object/table to query
+        this kwarg accepts a sqlalchemy mapped class as value
+
+        example: object=User -> will query the User table
+        """
         obj = kwargs.pop("object")
         self.db.query(obj).filter_by(**kwargs).first()
 
-    def get(self, **kwargs) -> List[Any]:
+    def get(self, **kwargs) -> List[Any] | None:
+        """
+        Get all objects from database using the specified kwargs as filters
+
+        The object kwarg is required to specify wich object/table to query
+        this kwarg accepts a sqlalchemy mapped class as value
+
+        example: object=User -> will query the User table
+        """
         obj = kwargs.pop("object")
         self.db.query(obj).filter_by(**kwargs).all()
