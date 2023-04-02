ee86d46d94e32168b58086aa74c62d7c3c7573f6
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2023-04-02 18:30:00 -0300

    feat: add authentication

--- src/routers/students_router.py
+++ src/routers/students_router.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter, Response
+import os
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, Response
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+from src.errors.auth_exception import NotAuthorized
 from src.services.students_service import StudentService
 
 router = APIRouter(prefix="/presenca")
@@ -7,9 +12,18 @@ router = APIRouter(prefix="/presenca")
 
 service = StudentService()
 
+security = HTTPBasic()
+username = os.getenv("ADMIN_USERNAME")
+password = os.getenv("ADMIN_PASSWORD")
+
 
 @router.post("/{cpf_number}", name="Adicionar presença")
-def add_attendance(cpf_number: str):
+def add_attendance(
+    cpf_number: str, credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    if credentials.username != username or credentials.password != password:
+        raise NotAuthorized("Usuário ou senha inválidos")
+
     service.add_attendance(cpf_number)
 
     return Response(status_code=201)
