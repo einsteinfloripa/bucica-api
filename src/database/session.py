96859d3a45d412827142c414e402c3370490e857
Gabriel Viana <viana_gab2001@hotmail.com>
2023-04-10 20:46:39 -0300

    fix: database pool connection (#50)

--- src/database/session.py
+++ src/database/session.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL", "")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
