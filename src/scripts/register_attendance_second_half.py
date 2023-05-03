c00288ecf28e9d6b2d391e8a24393bce6649e38b
Marcelo Antonio Ecco <92338508+maecco@users.noreply.github.com>
2023-05-03 20:06:05 -0300

    fix: scripts running twice per class (#75)
    * fix: change sleeptime from 1sec to 3sec
    
    * fix:correcting unintended change
    
    * fix: fix double running schedule by decorator
    
    ---------
    
    Co-authored-by: Gabriel Viana <viana_gab2001@hotmail.com>

--- src/scripts/register_attendance_second_half.py
+++ src/scripts/register_attendance_second_half.py
@@ -9,19 +9,13 @@ from src.utils.date_handler import DateHandler
 from src.utils.schedule import LateTypes
 
 
-@schedule.repeat(schedule.every().monday.at("22:01"))
-@schedule.repeat(schedule.every().tuesday.at("22:01"))
-@schedule.repeat(schedule.every().wednesday.at("22:01"))
-@schedule.repeat(schedule.every().thursday.at("22:01"))
-@schedule.repeat(schedule.every().friday.at("22:01"))
 def register_attendance():
-
     if DateHandler().is_holiday(datetime.now()):
-        print("Feriado, não serao resgistradas faltas para o segundo turno...")
+        print("Feriado, não serão registradas faltas para o segundo turno...")
         return
 
     print("Registrando faltas para o segundo turno...")
-    print('datetime: ', datetime.now())
+    print("datetime: ", datetime.now())
     db = SessionLocal()
 
     students = db.query(CadastroAlunos).all()
@@ -50,7 +44,13 @@ def register_attendance():
 
 
 def register_student_second_half():
+    schedule.every().monday.at("20:01").do(register_attendance)
+    schedule.every().tuesday.at("20:01").do(register_attendance)
+    schedule.every().wednesday.at("20:01").do(register_attendance)
+    schedule.every().thursday.at("20:01").do(register_attendance)
+    schedule.every().friday.at("20:01").do(register_attendance)
+
     print("Iniciado Job para registar faltas para o segundo turno...")
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(3)
