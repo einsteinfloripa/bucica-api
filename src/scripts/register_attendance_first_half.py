ae742cd2d65745191960dda0736e981d3c275e7f
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2023-05-04 07:11:21 -0300

    chore: change class times

--- src/scripts/register_attendance_first_half.py
+++ src/scripts/register_attendance_first_half.py
@@ -44,11 +44,11 @@ def register_attendance():
 
 
 def register_student_first_half():
-    schedule.every().monday.at("20:01").do(register_attendance)
-    schedule.every().tuesday.at("20:01").do(register_attendance)
-    schedule.every().wednesday.at("20:01").do(register_attendance)
-    schedule.every().thursday.at("20:01").do(register_attendance)
-    schedule.every().friday.at("20:01").do(register_attendance)
+    schedule.every().monday.at("18:21").do(register_attendance)
+    schedule.every().tuesday.at("18:21").do(register_attendance)
+    schedule.every().wednesday.at("18:21").do(register_attendance)
+    schedule.every().thursday.at("18:21").do(register_attendance)
+    schedule.every().friday.at("18:01").do(register_attendance)
 
     print("Iniciado Job para registar faltas para o primeiro turno...")
     while True:
