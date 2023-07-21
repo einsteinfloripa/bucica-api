cfc94a3899e08d9c26becad6a28d3fdaecb12816
Marcelo Ecco <marceloecco1@gmail.com>
2023-07-21 15:18:40 -0300

    chore: remove char limit on the name of the donor schema

--- src/schemas/donor_schema.py
+++ src/schemas/donor_schema.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class DonorSchema(BaseModel):
     name: str = Field(..., min_length=3, alias="nome")
-    indication: str = Field(..., min_length=3, alias="indicacao")
+    indication: str = Field(..., alias="indicacao")
     amount: float = Field(..., gt=0, alias="quantidade")
